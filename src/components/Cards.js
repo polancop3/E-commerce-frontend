2f629677fdcde330420e11b21ee8330edddba961
Pedro Polanco <pedropolanco@Pedros-MBP.lan1>
2021-11-20 19:52:14 -0500

    variables names fixed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,16 +4,16 @@ import { getItems } from './getItems';
 export default class Cards extends Component{
   constructor(props){
     super(props);
-    this.state = {movies: []}
+    this.state = {products: []}
     this.getProducts = this.getProducts.bind(this);
   }
 
   getProducts(){
         getItems()
-        .then(movies => {
-            console.log(movies)
+        .then(item => {
+            console.log(item)
             this.setState({
-                movies: movies
+                products: item
             })
         })
     }
@@ -27,7 +27,7 @@ export default class Cards extends Component{
 <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180" />
   <Card.Body>
-    <Card.Title>{this.state.movies.map((movie,index) =><p key={index}>{movie.title}</p>)}</Card.Title>
+    <Card.Title>{this.state.products.map((item, index) => <p key={index}>{item.title}</p>)}</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
